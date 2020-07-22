66f041ec6890029bd7ab29f2858ada13cf3c8044
LiuRui <liurui@xyd.cn>
2020-07-22 18:58:47 +0800

    修改插入interface表controller

--- src/main/java/com/platform/controller/InterFaceController.java
+++ src/main/java/com/platform/controller/InterFaceController.java
@@ -1,13 +1,20 @@
 package com.platform.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.platform.dal.mapper.platform.InterfaceMapper;
 import com.platform.dal.model.platform.Interface;
 import io.swagger.annotations.ApiOperation;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 @RestController
 @RequestMapping("interface/")
@@ -16,9 +23,30 @@ public class InterFaceController {
     @Autowired
     InterfaceMapper interfaceMapper;
 
+    @Resource
+    ObjectMapper objectMapper;
+
+    @SneakyThrows
     @PostMapping("add")
     @ApiOperation("添加新接口")
-    public void addInterFace(@RequestBody Interface inter){
-        interfaceMapper.insertSelective(inter);
+    public void addInterFace(@RequestBody Map<String,Object>  interfaceData){
+
+
+        Interface interfaceDTO;
+        //map转为实体类
+        interfaceDTO = JSON.parseObject(JSON.toJSONString(interfaceData),Interface.class);
+        for (Map.Entry<String, Object> entry:interfaceData.entrySet()){
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if("headerdetail".equals(key)){
+                interfaceDTO.setHeaderdetail(objectMapper.writeValueAsString(value));
+            }
+            if("body".equals(key)){
+                interfaceDTO.setBody(objectMapper.writeValueAsString(value));
+            }
+
+        }
+        System.out.println(interfaceDTO);
+        interfaceMapper.insertSelective(interfaceDTO);
     }
     }
